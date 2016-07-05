a26e3096e0df9e4f3f78836bc0a035df27bd7262
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-07-05 16:24:02 +0200

    SAVEPOINT

--- atelier-client/src/views/HomeView.jsx
+++ atelier-client/src/views/HomeView.jsx
@@ -3,6 +3,13 @@ import Hello from '../components/Hello';
 
 export default class HomeView extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+           message: 'hello from home'
+        };
+    }
+
     componentDidMount() {
         console.log('HomeView componentDidMount')
     }
@@ -11,11 +18,19 @@ export default class HomeView extends React.Component {
         console.log('HomeView componentDidUpdate')
     }
 
+    changeMessage() {
+        this.setState({
+            message: 'nouveau message'
+        });
+    }
+
     render() {
         console.log('HomeView render')
         return <div className="view-container">
             <h2>Home</h2>
-            <Hello message="hello from home"/>
+            <Hello message={this.state.message}/>
+            <br />
+            <button onClick={this.changeMessage}>Change message</button>
         </div>;
     }
 }
