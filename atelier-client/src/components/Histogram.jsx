b0e9a80480ee970612c2fec4a54cf049bec74e50
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-07-05 16:38:47 +0200

    SAVEPOINT

--- atelier-client/src/components/Histogram.jsx
+++ atelier-client/src/components/Histogram.jsx
@@ -11,6 +11,18 @@ export default class Histogram extends React.Component {
     render() {
         let max = Math.max(...this.props.data);
         return <svg width={1000} height={400}>
+            {this.props.data.map((d, index) =>
+                <g transform={`translate(${index * columnWidth + 20}, ${higherColumnHeight})`} key={index}
+                   styleName="column-group">
+                    <g transform="rotate(180)">
+                        <rect x={0} y={0}
+                              width={columnWidth}
+                              height={d / max * higherColumnHeight}
+                              styleName="column"/>
+                    </g>
+                    <text x={0} y={20} transform="rotate(45)" styleName="value">{d}</text>
+                </g>
+            )}
         </svg>
     }
 }
