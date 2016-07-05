057e764d7395898fa87452638dda38dd34e47a2d
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-07-05 16:16:19 +0200

    SAVEPOINT

--- atelier-client/src/components/Hello.jsx
+++ atelier-client/src/components/Hello.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 export default class Hello extends React.Component {
 
+    componentDidMount() {
+        console.log('Hello componentDidMount')
+    }
+
+    componentDidUpdate() {
+        console.log('Hello componentDidUpdate')
+    }
+
     render() {
+        console.log('Hello render')
         return <div>{this.props.message} <i className="fa fa-thumbs-up"/></div>;
     }
 
--- atelier-client/src/views/HomeView.jsx
+++ atelier-client/src/views/HomeView.jsx
@@ -3,7 +3,16 @@ import Hello from '../components/Hello';
 
 export default class HomeView extends React.Component {
 
+    componentDidMount() {
+        console.log('HomeView componentDidMount')
+    }
+
+    componentDidUpdate() {
+        console.log('HomeView componentDidUpdate')
+    }
+
     render() {
+        console.log('HomeView render')
         return <div className="view-container">
             <h2>Home</h2>
             <Hello message="hello from home"/>
